73a6e8e1f2514d0eee11c9284f3c154c2d73bc24
delimmaemma <calvinhtml@gmail.com>
2023-06-23 15:13:45 -0500

    Edited code to add onload listener

--- src/index.js
+++ src/index.js
@@ -23,30 +23,32 @@ function imgChange2() {
     adventureImg.src = 'https://camo.githubusercontent.com/2e0dd7a5cdc62e8987d18156acd5daa0c4209fafee5e5677ff9a2f8c64a8cdb3/68747470733a2f2f692e7974696d672e636f6d2f76692f524e6f48635745387462512f6d617872657364656661756c742e6a7067'
     console.log("You've been funkng SPOOPED")
 }
-
-Array.from(document.links).forEach(link => {
-    link.addEventListener('click', evt => {
-        evt.preventDefault
-        document.location.href = 'https://www.facebook.com'
+window.onload = function() {
+    Array.from(document.links).forEach(link => {
+        link.addEventListener('click', evt => {
+            evt.preventDefault
+            document.location.href = 'https://www.facebook.com'
+        })
     })
-})
 
-sunButton.addEventListener('click', (event) => {event.target.style.fontStyle = 'italic'})
-sunButton.addEventListener('click', (event) => {event.target.style.backgroundColor = 'maroon'})
-mountainButton.addEventListener('click', (event) => {event.target.style.fontStyle = 'italic'})
-mountainButton.addEventListener('click', (event) => {event.target.style.backgroundColor = 'maroon'})
-islandButton.addEventListener('click', (event) => {event.target.style.fontStyle = 'italic'})
-islandButton.addEventListener('click', (event) => {event.target.style.backgroundColor = 'maroon'})
-document.addEventListener('keydown', esc)
-document.addEventListener('keydown', any)
-document.addEventListener('copy', (event) => {event.target.style.fontSize = '2em'})
-document.addEventListener('cut', (event) => {event.target.style.color = 'purple'})
-document.addEventListener('paste', (event) => {event.target.style.fontFamily = 'Times New Roman'})
-document.addEventListener('dblclick', (event) => {event.target.style.fontFamily = 'Vivaldi'})
-document.addEventListener('dblclick', (event) => {event.target.style.fontSize = '3em'})
-adventureImg.addEventListener('mouseover', imgChange2)
-document.addEventListener('keyup', (event) => {event.target.style.backgroundColor = 'black'})
-document.addEventListener('blur', (event) => event.target.backgroundImage.src = 'https://t3.ftcdn.net/jpg/04/89/04/38/360_F_489043891_m4k5esAuIH7O44SIASSeRGtBuLiKoCA0.jpg')
-document.addEventListener('error', (event) => {event.target.style.color = 'red'})
-document.addEventListener('wheel', (event) => {event.target.style.fontWeight = 'bold'})
-busImg.addEventListener('click', imgChange)
+
+    sunButton.addEventListener('click', (event) => {event.target.style.fontStyle = 'italic'})
+    sunButton.addEventListener('click', (event) => {event.target.style.backgroundColor = 'maroon'})
+    mountainButton.addEventListener('click', (event) => {event.target.style.fontStyle = 'italic'})
+    mountainButton.addEventListener('click', (event) => {event.target.style.backgroundColor = 'maroon'})
+    islandButton.addEventListener('click', (event) => {event.target.style.fontStyle = 'italic'})
+    islandButton.addEventListener('click', (event) => {event.target.style.backgroundColor = 'maroon'})
+    document.addEventListener('keydown', esc)
+    document.addEventListener('keydown', any)
+    document.addEventListener('copy', (event) => {event.target.style.fontSize = '2em'})
+    document.addEventListener('cut', (event) => {event.target.style.color = 'purple'})
+    document.addEventListener('paste', (event) => {event.target.style.fontFamily = 'Times New Roman'})
+    document.addEventListener('dblclick', (event) => {event.target.style.fontFamily = 'Vivaldi'})
+    document.addEventListener('dblclick', (event) => {event.target.style.fontSize = '3em'})
+    adventureImg.addEventListener('mouseover', imgChange2)
+    document.addEventListener('keyup', (event) => {event.target.style.backgroundColor = 'black'})
+    document.addEventListener('blur', (event) => event.target.backgroundImage.src = 'https://t3.ftcdn.net/jpg/04/89/04/38/360_F_489043891_m4k5esAuIH7O44SIASSeRGtBuLiKoCA0.jpg')
+    document.addEventListener('error', (event) => {event.target.style.color = 'red'})
+    document.addEventListener('wheel', (event) => {event.target.style.fontWeight = 'bold'})
+    busImg.addEventListener('click', imgChange)
+}
